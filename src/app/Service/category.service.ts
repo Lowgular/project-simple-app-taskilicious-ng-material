61caf91159aa2fd920fd66f622c9cc4a38ef5e68
Olcaytp <olcaytapan10@gmail.com>
2023-02-20 13:35:51 +0100

    branch-created

---
+++ src/app/Service/category.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from "@angular/core";
+
+@Injectable({providedIn: 'root'})
+export class CategoryService {
+
+	baseURL: string = "https://63761992b5f0e1eb850298da.mockapi.io/categories";
+
+	//create a new category
+	createCategory(category: any) {
+	}
+
+	//get all categories
+	getCategories() {
+	}
+
+	//get a category by id
+	getCategory(id: any) {
+	}
+
+	//update a category
+	updateCategory(id: any, category: any) {
+	}
+
+	//delete a category
+	deleteCategory(id: any) {
+	}
+}
