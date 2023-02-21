c5dcc66a1af95f3839d9768f4353b2fa71bc7936
Mateusz <mateusz.jajkowicz@gmail.com>
2023-02-21 18:13:15 +0100

    Category Sort Dropdown

--- src/app/category/category-list/category-list.component.ts
+++ src/app/category/category-list/category-list.component.ts
@@ -11,13 +11,20 @@ import { Category } from 'src/app/shared/models/category';
 export class CategoryListComponent implements OnInit, OnDestroy {
   categories: Category[] = [];
   sub = new Subscription();
+  selected = 'ASC';
 
   constructor(private categoryService: CategoryService) {}
 
   ngOnInit(): void {
     this.sub = this.categoryService.getCategories().subscribe((categories) => {
-      (this.categories = categories.slice(0, 10)), console.log(this.categories);
+      (this.categories = categories.slice(0, 10)), this.filterByPriority();
     });
+  }
+
+  filterByPriority(asc = 1) {
+    var categories = this.categories;
+    categories.sort((a, b) => asc * a.name.localeCompare(b.name));
+    this.categories = [...categories];
   }
 
   ngOnDestroy(): void {
--- src/app/shared/materials.module.ts
+++ src/app/shared/materials.module.ts
@@ -3,8 +3,15 @@ import { MatListModule } from '@angular/material/list';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
 
 @NgModule({
-  exports: [MatListModule, MatButtonModule, MatIconModule, MatInputModule],
+  exports: [
+    MatListModule,
+    MatButtonModule,
+    MatIconModule,
+    MatInputModule,
+    MatSelectModule,
+  ],
 })
 export class MaterialsModule {}
